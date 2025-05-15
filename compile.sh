eaf09eafa2cb46cbf585e9e8fda2518e03a1967a
Elias Nacher Fernandez <eliasnacher@gmail.com>
2025-05-15 16:29:47 +0000

    Add compile bash, new icons and Play Store compilation

--- compile.sh
+++ compile.sh
@@ -1,1 +1,7 @@
-docker run --rm -v $(pwd):/app vue-capacitor-builder bash -c "./gradlew assembleDebug"
+sudo chown eliasnacher -R android
+npm run build
+npx cap sync android
+docker run --rm \
+  -v $(pwd):/app \
+  vue-capacitor-builder \
+  bash -c "./gradlew bundleRelease"
